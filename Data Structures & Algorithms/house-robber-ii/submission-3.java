e2deb30c9ae809f26d976d9655021d82d0402a72
gurijalaspandana2104 <gurijalaspandana2104@gmail.com>
2026-04-06 10:56:58 +0530

    Add: house-robber-ii - submission-3

---
+++ Data Structures & Algorithms/house-robber-ii/submission-3.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int rob(int[] nums) {
+        int n = nums.length;
+
+if (n == 1) return nums[0];
+if (n == 2) return Math.max(nums[0], nums[1]);
+        int[] dp1=new int[nums.length];
+        int[] dp2=new int[nums.length];
+        dp1[0]=nums[0];
+        
+        dp1[1]=Math.max(nums[0],nums[1]);
+         dp2[ 1]=nums[1];
+         dp2[ 2]=Math.max(nums[ 1],nums[2]);
+        for(int i= 2;i<nums.length-1;i++)
+        {
+            dp1[i]=Math.max(dp1[i-2]+nums[i],dp1[i-1]);
+        }
+        for(int i=3;i<nums.length;i++)
+        {
+            dp2[i]=Math.max(dp2[i-2]+nums[i],dp2[i-1]);
+        }
+        return Math.max(dp2[nums.length-1],dp1[nums.length-2]);
+    }
+}
