af992ceddb90630bab37c60b7ddba45b86cd287e
gurijalaspandana2104 <gurijalaspandana2104@gmail.com>
2026-06-05 10:25:12 +0530

    Add: single-number - submission-2

---
+++ Data Structures & Algorithms/single-number/submission-2.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int singleNumber(int[] nums) {
+        HashMap<Integer,Integer>hmap=new HashMap<>();
+        for(int i=0;i<nums.length;i++)
+        {
+            hmap.put(nums[i],hmap. getOrDefault(nums[i],0)+1);
+        }
+        for(Integer key:hmap.keySet())
+        {
+           int value=hmap.get(key);
+           if(value==  1)
+           {
+  
+            return key;
+           }
+        }
+       return -1;
+}
+} 
